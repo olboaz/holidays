971a9fbd8a5deb679ac163361ada308bb6ffaafd
patrice jaszczynski <ppastg92@free.fr>
2021-06-03 00:43:34 +0200

    initialize app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+
+    @places = Place.geocoded
+    @markers = @places.map do |place|
+
+    {
+      lat: place.latitude,
+      lng: place.longitude,
+      name: place.name,
+      user: place.user.email
+    }
+    end
+
   end
 end
---
+++ app/javascript/components/map.js
@@ -0,0 +1,29 @@
+const createMap = () => {
+
+  var carte = document.getElementById('mapid')
+  if(carte) {
+    var map = L.map('mapid').setView({lon: 2.209667, lat: 46.232193}, 6.3);
+
+    // add the OpenStreetMap tiles
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+
+    // show the scale bar on the lower left corner
+    L.control.scale().addTo(map);
+
+    // show a marker on the map
+    //L.marker({lon: 2.333333, lat: 48.866667}).bindPopup('Paris').addTo(map);
+    var markers = JSON.parse(carte.dataset.markers)
+    markers.forEach((marker) => {
+      L.marker({lon: marker.lng, lat: marker.lat}).bindPopup(`${marker.name}<br>${marker.user}`).addTo(map);
+
+    });
+
+
+  }
+
+}
+
+export {createMap};
